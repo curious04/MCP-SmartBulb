c6a70338fdbcc709973072d5d3212dd4edc8b75e
Hrithik Singh <96012516+curious04@users.noreply.github.com>
2025-06-01 07:11:37 +0000

    different services tools created

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import {
+    McpServer
+} from '@modelcontextprotocol/sdk/server/mcp.js';
+
+import {
+    StdioServerTransport
+} from '@modelcontextprotocol/sdk/server/stdio.js';
+
+import {
+    z
+} from 'zod';
+
+const server = new McpServer({
+    name: 'Philips Smart Bulb',
+    version: '1.0.0',
+    capabilities: {
+        resources: {},
+        tools: {},
+    },
+});
