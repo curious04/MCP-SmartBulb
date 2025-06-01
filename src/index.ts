53c0b866c4856d01d7635d58d9eb4fd64cc28650
Hrithik Singh <96012516+curious04@users.noreply.github.com>
2025-06-01 07:25:33 +0000

    all tools and transport for mcp server is created

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,12 @@ import {
     z
 } from 'zod';
 
+import {
+    turnOffBulb,
+    turnOnBulb,
+    changeColor
+} from './service';
+
 const server = new McpServer({
     name: 'Philips Smart Bulb',
     version: '1.0.0',
@@ -18,3 +24,60 @@ const server = new McpServer({
         tools: {},
     },
 });
+
+server.tool('turn-on-bulb', 'Turns the bulb on', async () => {
+    await turnOnBulb();
+    return {
+        content: [{
+            type: 'text',
+            text: 'Bulb has been turned on'
+        }]
+    };
+});
+
+server.tool('turn-off-bulb', 'Turns the bulb OFF', async () => {
+    await turnOffBulb();
+    return {
+        content: [{
+            type: 'text',
+            text: 'Bulb has been turned off'
+        }]
+    };
+});
+
+server.tool(
+    'change-bulb-color',
+    'Changes the color of the bulb', {
+        r: z.number().describe('Red Color of the light 0 255'),
+        g: z.number().describe('Green Color of the light 0 - 255'),
+        b: z.number().describe('Blue Color of the light 0 - 255'),
+        dimming: z.number().describe('Dimming of light where 0 means no light, 100 means full and 50 means 50% brightness'),
+    },
+    async ({
+        r,
+        g,
+        b,
+        dimming
+    }) => {
+        await changeColor({
+            b,
+            dimming,
+            g,
+            r,
+        });
+        return {
+            content: [{
+                type: 'text',
+                text: 'Bulb has been turned off'
+            }]
+        };
+    }
+);
+
+async function main() {
+    const transport = new StdioServerTransport();
+    await server.connect(transport);
+    console.error('Philips Bulb MCP Server running on stdio');
+}
+
+main();
